d21c95dd250954141132874f9511424476f034fa
tldms0507 <sieun050783@gmail.com>
2024-07-29 23:26:11 +0900

    [feat] 프로젝트 첫 세팅

---
+++ src/component/header/header.js
@@ -0,0 +1,13 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img src="/img/monamiLogo.png" alt="monami_logo"></img>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import Header from "../../component/header/header.js";
+
+function Home() {
+    return (
+        <div>
+            <Header/>
+            <div>메인페이지</div>
+        </div>
+    )
+}
+
+export default Home;
