d42d9fde5b45f37c026404c0d9bd6fb9515dfa7d
GeonWoo Jung <codelue01@gmail.com>
2024-07-30 16:54:14 +0900

    [feat] Login 구현중

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Home from "./pages/home/home.js";
+// import Login from "./pages/login/login.js";
+
 function App() {
   return (
     <div>
       <Home />
+      {/* <Login /> */}
     </div>
   );
 }
---
+++ src/pages/login/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            {/* <h1>Back ground img</h1> */}
+            <img src='img/monami_background.png' alt='ADSV'></img>
+        </div>
+    );
+};
+
+export default Login;
