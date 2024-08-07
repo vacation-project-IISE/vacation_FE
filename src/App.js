57c0bb08c8e180ac9c8c8fa482701322642e0744
GeonWoo Jung <codelue01@gmail.com>
2024-08-07 20:23:42 +0900

    [docs] login.css 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Home from "./pages/home/home.js";
-// import Login from "./pages/login/login.js";
+import Login from "./pages/login/login.js";
 
 function App() {
   return (
     <div>
       <Home />
-      {/* <Login /> */}
+      <Login />
     </div>
   );
 }
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import './'
 const Login = () => {
     return (
         <div>
             {/* <h1>Back ground img</h1> */}
             <img src='img/monami_background.png' alt='ADSV'></img>
         </div>
+        // <div className='back'></div>
     );
 };
+
 
 export default Login;
