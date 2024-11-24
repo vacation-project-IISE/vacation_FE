330fec82e68131876e5d31a319a445539a9bca94
Geonwoo Jung <codelue01@gmail.com>
2024-11-24 16:33:22 +0900

    [예비]

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,16 @@
 import Footer from "../../component/footer/footer.js";
 import Header from "../../component/header/header.js";
+import React, { useState } from "react";
 import "./home.css";
 
 function Home() {
+      // 상태 추가: 선택된 카테고리를 관리
+  const [activeCategory, setActiveCategory] = useState("premium-pen");
+
+  // 카테고리 변경 함수
+  const handleCategoryChange = (category) => {
+    setActiveCategory(category);
+  };
   return (
     <div>
       <Header />
@@ -45,18 +53,20 @@ function Home() {
             <span className="btn_inquiry">
                 "대량 구매 문의"
             </span>
-         </a>  
+         </a> 
+         <a className="l-popup__button--close" onClick={(e) => e.preventDefault()} href="#">예약하기 버튼 닫기</a>
       </div>
       <div className="new">
         <h2>NEW <br /> ARRIVALS</h2>
         <p>가장 먼저 만나는 설레는 기다림</p>
+        
       </div>
 
       <div className="newarrival">
-        <img src="img/zpen.jpg" className="home-zpen" alt="Z Pen" />
-        <img src="img/pluspen.jpg" className="home-pluspen" alt="Plus Pen" />
-        <img src="img/note.jpg" className="home-note" alt="Note" />
-        <img src="img/153pen.jpg" className="home-153pen" alt="153 Pen" />
+        <img src="img/zpen.jpg" className="home-zpen" alt width={392} height={205}/> 
+        <img src="img/pluspen.jpg" className="home-pluspen" alt width={392} height={292} />
+        <img src="img/note.jpg" className="home-note" alt width={786} height={360} />
+        <img src="img/153pen.jpg" className="home-153pen" width={450} height={439} />
         <img src="img/pluspen2.jpg" className="home-pluspen2" alt="Plus Pen 2" />
       </div>
 
@@ -64,19 +74,31 @@ function Home() {
         <div className="tit">Best Product</div>
         <p className="tit_btn">언제 어디서나 함께하는 모나미</p>
 
-        <div className="p_tabs">
-          <ul>
-            <li className="on"><a>프리미엄 펜</a></li>
-            <li><a>펜-펜슬</a></li>
-            <li><a>마카-컬러링</a></li>
-            <li><a>노트-사무용품</a></li>
-          </ul>
-        </div>
-        
-        <div className="p_lists">
-                        <ul className="wrapper_ul">
-                            <li>
-                                <a href="href=/product/product_view.php?idx=202&ccode=003005">
+        {/* 카테고리 선택 버튼 */}
+      <div className="p_tabs">
+        <ul>
+          <li className={activeCategory === "premium-pen" ? "on" : ""} onClick={() => handleCategoryChange("premium-pen")}>
+            <a>프리미엄 펜</a>
+          </li>
+          <li className={activeCategory === "pen-pencil" ? "on" : ""} onClick={() => handleCategoryChange("pen-pencil")}>
+            <a>펜-펜슬</a>
+          </li>
+          <li className={activeCategory === "marker-coloring" ? "on" : ""} onClick={() => handleCategoryChange("marker-coloring")}>
+            <a>마카-컬러링</a>
+          </li>
+          <li className={activeCategory === "notebook" ? "on" : ""} onClick={() => handleCategoryChange("notebook")}>
+            <a>노트-사무용품</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* 카테고리별 상품 리스트 */}
+      <div className="p_lists">
+        <ul className="wrapper_ul">
+          {activeCategory === "premium-pen" && (
+            <>
+              <li>
+              <a href="href=/product/product_view.php?idx=202&ccode=003005">
                                     <div className="thum">
                                 <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2040136001_file6_1713138750ujw6qaj97g.jpg" alt width={266} height={390}></img>
                                     </div>
@@ -154,10 +176,262 @@ function Home() {
                                         <div className="name">153 네오 만년필 EF</div>
                                     </div>
                                 </a>
-                            </li>
 
-                        </ul>
-                    </div>
+              </li>
+            </>
+          )}
+          {activeCategory === "pen-pencil" && (
+            <>
+              <li>
+                <a href="/product/product_view.php?idx=203&ccode=003006">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2100011114_file6_1680482799v8lz6c19ev.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">샤프/샤프심</div>
+                    <div className="name">그리픽스 팝 샤프</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2080201010_file6_1680510368gmw1j11ihk.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">사인펜 슈퍼 24색 세트</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2040240006_file6_16255408900kpz876plz.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">프러스펜 3000 피그먼트</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2040016351_file6_16334947342g3aqx0uh1.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">라이브 칼라(화이트축)</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2010152034_file6_1699945497e4wvhszoxy.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">유성볼펜</div>
+                    <div className="name">Fx ZETA C3</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2010030001_file6_1635127393stijdjvr99.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">유성볼펜</div>
+                    <div className="name">제니스7</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2041019049_file6_1659673523b6tw8za4y1.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">프러스펜 3000 60색 세트</div>
+                  </div>
+                </a>
+              </li>
+              
+              
+            </>
+          )}
+ {activeCategory === "marker-coloring" && (
+            <>
+              <li>
+                <a href="/product/product_view.php?idx=203&ccode=003006">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2040002001_file6_15867537131mu8wvyj4p.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">워터브러쉬</div>
+                    <div className="name">워터 리얼 브러쉬</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2040005001_file6_1585872127mz32ryzzpt.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">컬러 리얼 브러쉬</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2040012004_file6_161526431756qs7ja5jo.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">붓펜 12색 세트</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2040009001_file6_1552521391bpvzh1xb2b.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성펜</div>
+                    <div className="name">컬러 트윈 브러쉬</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2170001334_file6_1523248839zec883ldj9.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">크레파스 </div>
+                    <div className="name">크레파스_모니주(옐로우)</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2170001335_file6_1523248634tsel5la94n.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">크레파스</div>
+                    <div className="name">크레파스_모니주(핑크)</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2041019049_file6_1659673523b6tw8za4y1.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">프러스펜 3000 60색 세트</div>
+                  </div>
+                </a>
+              </li>
+              
+              
+            </>
+          )}
+          {activeCategory === "notebook" && (
+            <>
+              <li>
+                <a href="/product/product_view.php?idx=203&ccode=003006">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2120014201_file6_1711340757johyd13i1d.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">노트</div>
+                    <div className="name">지퀀스 노트 The Color A5</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2172018015_file6_15704371548jbp9f11m5.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">풀</div>
+                    <div className="name">다목적으로 사용 가능한 목공풀</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2172018017_file6_1570436926c8ouqp5opo.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">풀</div>
+                    <div className="name">다목적으로 사용 가능한 목공풀 2-Way</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2172018013_file6_1570436889l87efqci2x.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">풀</div>
+                    <div className="name">빨리 마르는 물풀</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2172018011_file6_1570436859fjhnr74sr9.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">풀 </div>
+                    <div className="name">붙였다 뗄 수 있는 메모 풀</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2172018005_file6_1570436803n3uw1ntrmc.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">풀</div>
+                    <div className="name">모서리에 바르기 쉬운 삼각풀</div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/product_view.php?idx=204&ccode=003007">
+                  <div className="thum">
+                    <img src="https://dfrkkcv2hg1jc.cloudfront.net/data/product/2041019049_file6_1659673523b6tw8za4y1.jpg" alt="" width={266} height={390} />
+                  </div>
+                  <div className="info">
+                    <div className="cate">수성마카</div>
+                    <div className="name">프러스펜 3000 60색 세트</div>
+                  </div>
+                </a>
+              </li>
+              
+              
+            </>
+          )}
+          {/* 다른 카테고리들에 대해서도 같은 방식으로 추가 */}
+        </ul>
+      </div>
         <div className="video">
           <video className="player" autoPlay muted loop>
             <source src="https://dfrkkcv2hg1jc.cloudfront.net/data/video/monami_brand_web_60.mp4" type="video/mp4" />
